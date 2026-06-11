eab99f63558df261442f40eca2014a78b4e14c9a
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2026-06-11 00:00:16 -0300

    fix: variable de entorno y api service

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,6 +1,6 @@
 import { type UsuarioAuth } from "../types/Auth";
 
-const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const api = {
   // Auth
